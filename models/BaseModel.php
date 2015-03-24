724aef8191cb6a73abc79122b9ed96b5e45b6e78
bangbang93 <bangbang93@163.com>
2015-03-24 15:16:55 +0800

    完成数据库语句层

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -23,8 +23,106 @@ abstract class BaseModel {
         }
     }
 
+    /**
+     * @param $config
+     */
     static public function init($config){
         self::$config = $config;
         self::$pdo = new PDO('mysql:host='.$config['host'].';dbname='.$config['db'], $config['user'], $config['password']);
+    }
+
+    /**
+     * @param array $data
+     * @return PDOStatement
+     */
+    public function insert(array $data){
+        $col = [];
+        $placeholder = [];
+        $value = [];
+        foreach ($data as $k=>$v){
+            $col[] = $k;
+            $value[] = $v;
+            $placeholder[] = '?';
+        }
+        $sql = "INSERT INTO $this->table (`".implode('`,`', $col)."`) VALUES (".implode(',', $placeholder).")";
+        $stmt = self::$pdo->prepare($sql);
+        $stmt->execute($value);
+        return $stmt;
+    }
+
+    /**
+     * @param array $col
+     * @param array $data
+     * @return PDOStatement
+     */
+    public function select(array $col, array $data = null){
+        $sql = "SELECT (`".implode('`,`', $col)."`) FROM `$this->table`";
+        $value = [];
+        if (!empty($data)){
+            $where = [];
+            foreach($data as $k=>$v){
+                $value[] = $v;
+                $where[] = "`$k` = ?";
+            }
+            if (!empty($where))
+                $sql .= implode(' AND ', $where);
+        }
+        $stmt = self::$pdo->prepare($sql);
+        $stmt->execute($value);
+        return $stmt;
+    }
+
+    /**
+     * @param $col |$data
+     * @param array $data
+     * @return string
+     */
+    public function count($col, array $data = null){
+        if (is_array($col)){
+            $data = $col;
+            $col = '*';
+        }
+        $sql = "SELECT count($col) FROM `$this->table`";
+        $value = [];
+        if (!empty($data)){
+            $where = [];
+            foreach($data as $k=>$v){
+                $value[] = $v;
+                $where[] = "`$k` = ?";
+            }
+            if (!empty($where))
+                $sql .= implode(' AND ', $where);
+        }
+        $stmt = self::$pdo->prepare($sql);
+        $stmt->execute($value);
+        return $stmt->fetchColumn();
+    }
+
+    /**
+     * @param array $data
+     * @param array $where
+     * @return PDOStatement
+     */
+    public function update(array $data, array $where = null){
+        $sql = "UPDATE `$this->table` SET ";
+        $col = [];
+        $value = [];
+        foreach($data as $k=>$v){
+            $value[] = $v;
+            $col[] = "`$k` = ?";
+        }
+        $sql .= implode(',', $col);
+        if (!empty($where)){
+            $sql .= ' WHERE ';
+            $col = [];
+            foreach($where as $k=>$v){
+                $value[] = $v;
+                $col[] = "`$k` = ?";
+            }
+            $sql .= implode(' AND ', $col);
+        }
+        $stmt = $this->pdoStmt = self::$pdo->prepare($sql);
+        $stmt->execute($value);
+        return $stmt;
     }
 }
--- models/UserModel.php
+++ models/UserModel.php
@@ -9,9 +9,41 @@
 class UserModel extends BaseModel{
     public function __construct(){
         parent::__construct();
+        $this->table = self::$config['prefix'].'users';
     }
 
-    public function getUserByUid(){
+    public function getUserByUid($uid){
+        $stmt = $this->pdoStmt = self::$pdo->prepare("SELECT * FROM $this->table WHERE `uid` = :uid");
+        $stmt->execute([':uid'=>$uid]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 
+    public function add($nickname, $password, $email, $phone, $realname){
+        $stmt = $this->pdoStmt = self::$pdo->prepare("INSERT INTO $this->table (`nickname`, `password`, `email`, `phone`, `realname`) VALUES (:nickname, :password, :email, :phone, :realname)");
+        $stmt->execute([
+            ':nickname'=>$nickname,
+            ':password'=>$password,
+            ':email'=>$email,
+            ':phone'=>$phone,
+            ':realname'=>$realname
+        ]);
+        if ($stmt->errorCode()!= '00000'){
+            throw new Exception($stmt->errorInfo());
+        }
+    }
+
+    public function getUserByEmail($email){
+        $stmt = $this->pdoStmt = self::$pdo->prepare("SELECT * FROM $this->table FROM `email` = :email");
+        $stmt->execute([':email'=>$email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function update($uid, $field, $value = null){
+        if (!is_array($field)){
+            $old_field = $field;
+            $field = [];
+            $field[$old_field] = $value;
+        }
+        $stmt = $this->pdoStmt = self::$pdo->prepare("UPDATE $this->table SET ");
     }
 }
